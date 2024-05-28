9039eda8650881eeead3300d4b1781f689dbdef8
Pedro Ferreira <116006000+Fipura@users.noreply.github.com>
2024-05-28 16:12:51 +0100

    Atualização do main.py
    Alterações do main.py para ler os dados do BME280

--- main.py
+++ main.py
@@ -1,101 +1,87 @@
-from machine import ADC, Pin, I2C
+from machine import Pin, I2C
 import wifimgr
 import time
 import machine
 from umqtt.simple import MQTTClient
 import utime
+import BME280
 
-# LED
+# Configura o pino LED como saída
 led = machine.Pin("LED", machine.Pin.OUT)
 
-#Calibraton values
-min_moisture=19200
-max_moisture=49300
-upper_threshold = 32.5
-lower_threshold = 28.5
+# Inicializa a interface I2C com os pinos SCL e SDA especificados
+i2c = I2C(id=0, scl=Pin(5), sda=Pin(4), freq=10000)
 
-readDelay = 10 # delay between readings
+# Inicializa o sensor BME280 utilizando a interface I2C
+bme = BME280.BME280(i2c=i2c)
 
-Moisture_Pin = 26
-soil = ADC(Pin(Moisture_Pin)) # Soil moisture PIN reference
+# Define o intervalo de leitura em segundos
+readDelay = 10 
 
+# Conecta-se à rede WiFi utilizando a biblioteca wifimgr
 wlan = wifimgr.get_connection()
 if wlan is None:
-    print("Não foi possível iniciar a ligação")
+    # Se a conexão falhar, imprime uma mensagem de erro e entra num ciclo infinito
+    print("Failed to initialize connection")
     while True:
         pass
-    
-print("OK")
-print(wlan)
-led_state = "OFF"
-led.low()
 
-adcpin = 4
-sensor = machine.ADC(adcpin)
+print("WiFi Connected")
 
-mqtt_server = '2.tcp.eu.ngrok.io'
+# Configurações do servidor MQTT
+mqtt_server = '5.tcp.eu.ngrok.io'
 client_id = 'teste'
 topic_pub = b'test'
-topic_port = '16693'
-is_client_message = False
+led_state = "OFF"
+led.low()  # Desliga o LED inicialmente
 
+# Função de callback para processar mensagens recebidas pelo MQTT
 def mqtt_callback(topic, msg):
     global led_state
     print("Received message: %s" % msg)
     if msg == b"ON":
-        led.high()
+        led.high()  # Liga o LED
         led_state = "ON"
     elif msg == b"OFF":
-        led.low()
+        led.low()  # Desliga o LED
         led_state = "OFF"
 
+# Função para conectar ao servidor MQTT
 def mqtt_connect():
-    client = MQTTClient(client_id, mqtt_server, port=16693, user="Pedro", password="test",  keepalive=3600)
-    client.set_callback(mqtt_callback)
-    client.set_last_will("test", "Disconnected")
-    client.connect()
-    print('Connected to %s MQTT Broker'%(mqtt_server))
-    client.subscribe(topic_pub)
+    client = MQTTClient(client_id, mqtt_server, port=10870, user="Pedro", password="test", keepalive=3600)
+    client.set_callback(mqtt_callback)  # Define a função de callback para mensagens MQTT
+    client.set_last_will("test", "Disconnected")  # Define a mensagem de última vontade
+    client.connect()  # Conecta ao servidor MQTT
+    print('Connected to %s MQTT Broker' % (mqtt_server))
+    client.subscribe(topic_pub)  # Subscreve ao tópico definido
     return client
 
+# Função para tentar reconectar ao servidor MQTT em caso de falha
 def reconnect():
     print('Failed to connect to the MQTT Broker. Reconnecting...')
     time.sleep(5)
-    machine.reset()
-    
+    machine.reset()  # Reinicia a máquina
+
+# Tenta conectar ao servidor MQTT, e se falhar, tenta reconectar
 try:
     client = mqtt_connect()
 except OSError as e:
     reconnect()
 
-last_send_time = utime.time()
+last_send_time = utime.time()  # Armazena o tempo atual
 
+# Ciclo principal
 while True:
-    #print("Sending messages")
-    client.check_msg()
-    current_time = utime.time()
-    
+    client.check_msg()  # Verifica mensagens recebidas pelo MQTT
+    current_time = utime.time()  # Obtém o tempo atual
+
+    # Se o tempo decorrido for maior ou igual ao intervalo de leitura
     if current_time - last_send_time >= readDelay:
-        moisture = (max_moisture-soil.read_u16())*100/(max_moisture-min_moisture)
-        if moisture <= upper_threshold:
-            if led_state == "OFF":
-                led_state = "ON"
-                led.high()  
-        elif moisture >= lower_threshold:
-            if led_state == "ON":
-                led_state = "OFF"
-                led.low() 
-        topic_msg = str("%.2f" % moisture +"%" + led_state)
-        print("moisture: " + "%.2f" % moisture +"% (adc: "+str(soil.read_u16())+")")
-        client.publish(topic_pub, topic_msg, retain=True)
-        last_send_time = current_time
-          
-    #utime.sleep(1) # set a delay between readings
-    
-    
-    #led.high()
-    #time.sleep(1)
-    #led.low()
-    #time.sleep(1)
-    
-        
+        tempC = bme.temperature  # Lê a temperatura do sensor
+        hum = bme.humidity  # Lê a humidade do sensor
+        pres = bme.pressure  # Lê a pressão do sensor
+        # Formata a mensagem a ser enviada com os valores lidos e o estado do LED
+        topic_msg = "{}/{}/{}/{}".format(tempC, hum, pres, led_state)
+        print(topic_msg)  # Imprime a mensagem no terminal
+        client.publish(topic_pub, topic_msg, retain=True)  # Publica a mensagem no tópico MQTT
+        last_send_time = current_time  # Atualiza o tempo da última leitura
